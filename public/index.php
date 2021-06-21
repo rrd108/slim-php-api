5bd5785d689bda38986ad1b60011e48944cf925a
rrd <rrd@1108.cc>
2021-06-21 17:23:48 +0200

    handling /users

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/db.php';
 
 $app = AppFactory::create();
 $app->setBasePath('/~rrd/slim-php-api');        // http://localhost/~rrd/slim-php-api/
@@ -11,6 +12,19 @@ $app->setBasePath('/~rrd/slim-php-api');        // http://localhost/~rrd/slim-ph
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Gauranga!");
     return $response;
+});
+
+$app->get('/users', function (Request $request, Response $response, $args) {
+  $db = new DB();
+  $pdo = $db->connect();
+  $stmt = $pdo->prepare('SELECT * FROM users');
+  $stmt->execute();
+  $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  // TODO Implement auth
+  $response->getBody()->write(json_encode($data));
+  return $response
+    ->withHeader('content-type', 'application/json')
+    ->withStatus(200);
 });
 
 $app->run();
